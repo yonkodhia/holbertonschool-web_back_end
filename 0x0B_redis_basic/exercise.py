a2f30cc061f1e1ed2efbbe4c95b215623056b67c
cherni dhia <70884179+yonkodhia@users.noreply.github.com>
2022-11-09 14:02:28 +0100

    task 2

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -27,6 +27,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
         
+      @call_history 
      @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store the input data in Redis using a
