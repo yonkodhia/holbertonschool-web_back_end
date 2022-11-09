ab7c9f1392872f21612e3ebf52a2c27b556018fb
cherni dhia <70884179+yonkodhia@users.noreply.github.com>
2022-11-09 14:00:43 +0100

    task 2

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -26,6 +26,16 @@ class Cache:
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
+        
+     @count_calls
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Store the input data in Redis using a
+        random key and return the key.
+        """
+        random_key = str(uuid4())
+        self._redis.set(random_key, data)
+
+        return random_key
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         random_key = str(uuid4())
