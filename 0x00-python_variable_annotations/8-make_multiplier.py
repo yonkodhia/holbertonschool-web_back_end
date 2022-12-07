75f6f251c30d783a755c6ed8a7c6b9d2727ecfb3
cherni dhia <70884179+yonkodhia@users.noreply.github.com>
2022-12-07 13:37:43 +0100

    mmmm

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""a type-annotated function make_multiplier"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """make multiplier"""
+    def multiply(x):
+        return x * multiplier
+    return multiply
