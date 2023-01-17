05537bc58acb81aab88a0be04418ef96880f92a3
cherni dhia <70884179+yonkodhia@users.noreply.github.com>
2023-01-17 14:00:11 +0100

    throw

---
+++ 0x0F-ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+
+export default function guardrail(mathFunction) {
+  const queue = [];
+  let tmp;
+  try {
+    tmp = mathFunction();
+  } catch (error) {
+    tmp = error.toString();
+  }
+  queue.push(tmp);
+  queue.push('Guardrail was processed');
+  return queue;
+}
